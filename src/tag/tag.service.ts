0f0d694d8852810511d0f626517cebe1c3735157
hel1_yeah <hel1_yeah@ukr.net>
2025-02-06 22:08:19 +0200

    add nodemon for alis

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class TagService<T> {
   getAllTags(): T[] {
-    return ['dragon', 'unicorn', 'three', 'coffee'] as T[];
+    return ['dragon', 'unicossssssrn', 'three', 'coffee'] as T[];
   }
 }
