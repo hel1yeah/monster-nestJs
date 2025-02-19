b0f318ddec71dacaa6f181aea5593b92e359c0cf
hel1_yeah <hel1_yeah@ukr.net>
2025-02-19 16:21:53 +0200

    remove test module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,13 @@ import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
-import { TagTestModule } from '@app/tagTest/tag.module';
 import { UserModule } from '@app/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import ormconfig from '@app/ormconfig';
 import AuthMiddlewares from './user/middlewares/auth.middlewares';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule, TagTestModule],
+  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
