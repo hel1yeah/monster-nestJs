ff19c348f5101fa71d52be31c31690a214b20e8e
hel1_yeah <hel1_yeah@ukr.net>
2025-02-19 22:10:07 +0200

    get article with slug

--- src/articles/article.controller.ts
+++ src/articles/article.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Post, UseGuards, Body } from '@nestjs/common';
+import { Controller, Post, Get, UseGuards, Body, Query, Param } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { AuthGuard } from '@app/user/guards/auth.guard';
 import { User } from '@app/user/decorators/user.decorators';
 import { UserEntity } from '@app/user/user.entity';
 import { CreateArticleDto } from './dto/createArticle.dto';
 import { ArticleResponseInterface } from './types/articleResponse.interface';
+import { log } from 'console';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articlesService: ArticleService) {}
+
+  @Get(':slug')
+  async getSingleArticle(@Param('slug') slag: string): Promise<ArticleResponseInterface> {
+    const article = await this.articlesService.getArticleBySlug(slag);
+    return this.articlesService.buildArticleResponse(article);
+  }
 
   @UseGuards(AuthGuard)
   @Post()
